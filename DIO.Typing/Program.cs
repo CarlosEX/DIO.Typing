ede4dc97bb611ffa81d89f468c0617a70a3f7cd5
Carlos Antonio <carlos22martinssilva@gmail.com>
2021-08-23 20:04:45 -0300

    Adicionar arquivos de projeto.

---
+++ DIO.Typing/Program.cs
@@ -0,0 +1,17 @@
+﻿using DIO.Typing.View;
+using System;
+using System.Windows.Forms;
+
+namespace DIO.Typing {
+    static class Program {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormHome());
+        }
+    }
+}
---
+++ DIO.Typing/View/FormHome.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Forms;
+
+namespace DIO.Typing.View {
+    public partial class FormHome : Form {
+        public FormHome() {
+            InitializeComponent();
+        }
+    }
+}
